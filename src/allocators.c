27612871180e4f2999331b98a3d9a5bc37e3ccbd
Ger Hobbelt <ger@hobbelt.com>
2024-08-28 00:39:31 +0200

    fix for MSVC dbgcrt.h usage in debug builds: provide static/local minimal wrappers for the RTL malloc/free calls to ensure their interface matches with the callback function protoypes they will be assigned to.

--- src/allocators.c
+++ src/allocators.c
@@ -7,9 +7,22 @@
 
 #include "cbor/common.h"
 
-CBOR_EXPORT _cbor_malloc_t _cbor_malloc = malloc;
-CBOR_EXPORT _cbor_realloc_t _cbor_realloc = realloc;
-CBOR_EXPORT _cbor_free_t _cbor_free = free;
+// wrap RTL calls in static functions: fixes issues with MSVC crtdbg.h usage, DLL vs static/lib RTL exports, etc.
+
+static void *my_cbor_malloc(size_t s) {
+	return malloc(s);
+}
+static void *my_cbor_realloc(void *p, size_t s) {
+	return realloc(p, s);
+}
+static void my_cbor_free(void *p) {
+  if (p != NULL)
+		free(p);
+}
+
+CBOR_EXPORT _cbor_malloc_t _cbor_malloc = my_cbor_malloc;
+CBOR_EXPORT _cbor_realloc_t _cbor_realloc = my_cbor_realloc;
+CBOR_EXPORT _cbor_free_t _cbor_free = my_cbor_free;
 
 void cbor_set_allocs(_cbor_malloc_t custom_malloc,
                      _cbor_realloc_t custom_realloc, _cbor_free_t custom_free) {
